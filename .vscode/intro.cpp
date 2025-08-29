7ea03c99d3be7bd1a6a84db25707f30b5862b98a
sukhmansingh65 <sukhmansingh01111@gmail.com>
2025-08-29 09:20:45 +0530

    My first program

---
+++ .vscode/intro.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"Hello World! This is my first program\n";
+     int age =18;
+
+     cout<<"My age is "<<age<<endl;
+
+     char name[20];
+     cout<<"Enter your Name";
+     cin>>name;
+
+     cout<<"My name is "<<name<<endl;
+
+     int a=5;
+     int b=9;
+     cout<<"The sum of a and b is"<<a+b<<endl;
+    
+
+
+
+return 0;
+
+}
