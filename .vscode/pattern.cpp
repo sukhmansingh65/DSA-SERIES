9d70b31ca833a24fa2064056f90925fe922ac987
sukhmansingh65 <sukhmansingh01111@gmail.com>
2025-09-12 00:09:15 +0530

    Patterns

--- .vscode/pattern.cpp
+++ .vscode/pattern.cpp
@@ -49,19 +49,62 @@
 // }
 
 // 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char ch='A';
+//     for(int i=0;i<4;i++)
+//     {
+//         for(int j=0;j<=i;j++)
+//         {
+//             cout<<ch<<"\t";
+//             ch++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char ch='A';
+//    for(int i=0;i<3;i++)
+// {
+//     for(int j=0;j<i+1;j++)
+//     {
+// cout<<ch;
+// ch++;
+//     }
+//     cout<<endl;
+// }
+// }
+
+//Pyramid pattern
 #include<iostream>
 using namespace std;
 int main()
 {
-    char ch='A';
-    for(int i=0;i<4;i++)
+    int n=9;
+    //outer loop
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<=i;j++)
+        //spaces
+        for(int j=0;j<=n-i-1;j++)
         {
-            cout<<ch<<"\t";
-            ch++;
+            cout<<" ";
+        }
+        //for numbers
+        for(int j=1;j<=i+1;j++)
+        {
+            cout<<j;
+        }
+        for(int j=i;j>=1;j--)
+        {
+            cout<<j;
         }
         cout<<endl;
     }
-    return 0;
 }
